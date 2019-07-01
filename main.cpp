9c9795da815fc75fc4e5216e05050ace7092a5d8
brrcrites <brrcrites@gmail.com>
2019-06-30 17:13:17 -0700

    Add basic main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "bash.hpp"
+
+int main() {
+    // This is a very basic main, and being able to correctly execute this main
+    // does not constitute a completed lab. Make sure you write unit tests for
+    // all the classes that you create (and can be instantiated) in this lab
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Base* four = new Op(4);
+    Base* two = new Op(2);
+    Base* mult = new Mult(seven, four);
+    Base* add = new Add(three, mult);
+    Base* minus = new Sub(add, two);
+
+    std::cout << minus->stringify() << " = " << minus->evaluate() << std::endl;
+    return 0;
+}
